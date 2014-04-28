cefed859af17101e445965c930ed93e52b435bec
Sebastian Krebs <krebs.seb@gmail.com>
2014-04-28 23:34:12 +0200

    PSR-4

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace Crunch\FastCGI;
 
-class DummyTest extends \PHPUnit_Framework_TestCase {
-    public function testDummy () {
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy ()
+    {
         $this->markTestIncomplete('Implement');
     }
 }
