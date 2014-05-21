9391f8ef909dd296cf96d421dd122599c9f8537f
Sebastian Krebs <krebs.seb@gmail.com>
2014-05-21 22:00:44 +0200

    Initial testing environment

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -5,6 +5,24 @@ class DummyTest extends \PHPUnit_Framework_TestCase
 {
     public function testDummy ()
     {
-        $this->markTestIncomplete('Implement');
+        $client = new Client('unix:///var/run/php5-fpm.sock');
+        $connection = $client->connect();
+        $request = $connection->newRequest(array(
+            'Foo' => 'Bar', 'GATEWAY_INTERFACE' => 'FastCGI/1.0',
+            'REQUEST_METHOD'                    => 'POST',
+            'SCRIPT_FILENAME'                   => __DIR__ . '/Resources/scripts/echo.php',
+            'CONTENT_TYPE'                      => 'application/x-www-form-urlencoded',
+            'CONTENT_LENGTH'                    => strlen('foo=bar')
+        ), 'foo=bar');
+
+        $connection->sendRequest($request);
+        $response = $connection->receiveResponse($request);
+        $response = $connection->receiveResponse($request);
+
+        list($header, $body) = explode("\r\n\r\n", $response->content);
+
+        list($server) = json_decode($body, true);
+
+        $this->assertEquals(7, $server['CONTENT_LENGTH']);
     }
 }
